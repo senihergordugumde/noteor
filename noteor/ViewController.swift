1f81968354d3ce85bc370ad5ff5500f9956a28be
Emir <emiraksu1608@gmail.com>
2024-02-17 14:55:11 +0300

    Initial Commit

---
+++ noteor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  noteor
+//
+//  Created by Emir AKSU on 17.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
